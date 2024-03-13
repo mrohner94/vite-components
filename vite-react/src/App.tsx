dc568a4659ce2692ffb6820afb635c233dd7e1a4
mrohner94 <87048324+mrohner94@users.noreply.github.com>
2024-03-13 11:42:37 -0400

    module federation / loading component from server

--- vite-react/src/App.tsx
+++ vite-react/src/App.tsx
@@ -1,17 +1,16 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import { Button, Input } from "vite-components";
+import { Suspense, useState } from 'react';
+import './App.css';
+import { lazy } from 'react';
+
+const Button = lazy(() => import('femds/button'));
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <Input />
-      <Button dangerous>Button</Button>
-    </>
+    <Suspense>
+      <Button>Hello</Button>
+    </Suspense>
   );
 }
 
